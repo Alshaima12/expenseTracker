9ff452f5bef183c6a9fbac10351118726a468db5
Alshaima <26J2013@utas.edu.om>
2025-12-09 12:18:45 +0400

    Your commit message

--- lib/user_register.dart
+++ lib/user_register.dart
@@ -136,7 +136,7 @@ class _UserRegisterState extends State<UserRegister> with SingleTickerProviderSt
     if (value == null || value.isEmpty) {
       return 'Please enter your email address';
     }
-    if (!RegExp(r'(?i)\b[0-9]{2,3}[J|j|S|s][0-9]+@utas.edu.om\b').hasMatch(value)) {
+    if (!RegExp(r'\b[0-9]{2,3}[J|j|S|s][0-9]+@utas.edu.om\b').hasMatch(value)) {
       return 'Please enter a valid email address in the format "xxJ/Sxxxx@utas.edu.om"';
     }
     return null;
